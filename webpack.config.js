f5d4cec830015cfe314ffa9caf5f453e74b2391c
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2023-02-09 03:08:22 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InterpolateHtmlPlugin = require("interpolate-html-plugin");
+
+module.exports = ({ mode } = { mode: "production" }) => {
+  console.log(`mode is: ${mode}`);
+
+  return {
+    mode,
+    entry: "./src/index.js",
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, "build"),
+      filename: "bundled.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+          exclude: /node_modules/,
+          use: ["url-loader", "file-loader"]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css|scss$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    watchOptions: {
+      ignored: '**/node_modules'
+    },
+    plugins: [
+      new InterpolateHtmlPlugin({
+        PUBLIC_URL: 'static' // can modify `static` to another name or get it from `process`
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        manifest: "./public/manifest.json"
+      })
+    ],
+    devServer: {
+      open: true
+    }
+  }
+};
