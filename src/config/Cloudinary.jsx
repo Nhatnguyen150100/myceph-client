5140e45e31ed439954ef6b1fe06466d293b52e95
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2023-07-12 10:24:10 +0700

    ignore config and analysis

--- src/config/Cloudinary.jsx
+++
@@ -1,3 +0,0 @@
-export const CLOUDINARY_KEY = '621123368244999';
-export const CLOUDINARY_SECRET = 'bg5jrQ0D-YvLgDTDU1fKtFqCyMI';
-export const CLOUDINARY_BASE_URL = 'https://api.cloudinary.com/v1_1/dvzgiho5t';
